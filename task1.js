a39dad363cf4b9a12d9964fe78a1ac27e6555714
pawanvaswani19931 <vaswanipawan.348@gmail.com>
2024-05-06 12:33:43 +0530

    my first name

---
+++ task1.js
@@ -0,0 +1,17 @@
+// let arr= ["junvary, febery, March, April"]
+// console.log(arr.splice(3,0,"Decmber"));
+// console.log(arr);
+
+// let arr= [5, 65, 48, 42, 43, 189]
+// console.log(arr.reduce(a,b,c,d));
+
+let burgerg = document.querySelector('.Pizza')
+let rightnav = document.querySelector('.rightNav')
+let navlist = document.querySelector('.nav-list')
+let navbar = document.querySelector('.navbar')
+
+burgerg.addEventListener('click',(Pizza)=>{
+    rightnav.classlist.toggle("v-class-resp")
+    navlist.classlist.toggle("'v-class-resp'")
+    navbar.classlist.toggle("h-nav-resp")
+})
